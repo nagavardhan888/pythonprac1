cfc4c8f0da5aef0f088155364c8de36ac77db830
nagavardhan888 <nagavardhan@gmail.com>
2026-04-18 00:07:18 +0530

    some more problems

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-# Calculating Plant Growth Stages
-n = int(input("enter the number of days: "))
-n1=0;
-n2 =1;
-print(n1,n2,end=" ")
-for i in range(2,n+1):
-    n = n1+n2
-    n1=n2
-    n2=n
-    print(n,end=" ")
+# #########Calculating Plant Growth Stages
+# n = int(input("enter the number of days: "))
+# n1=0;
+# n2 =1;
+# print(n1,n2,end=" ")
+# for i in range(2,n+1):
+#     n = n1+n2
+#     n1=n2
+#     n2=n
+#     print(n,end=" ")
+########## coding marathon 
+# n = int(input("number of participants: "))
+# k = int(input("number of top participants: "))
+# marks =  list(map(int, input("enter the marks of participants: ").split()))
+# marks.sort(reverse=True)
+# res = sum(marks[:k])
+# print(res)
+# ##############armstrong number
+# n = int(input("enter the number:"))
+# d1 = n//100
+# d2 = (n//10)%10
+# d3 =n%10
+# res = d1**3 +d2**3+d3**3
+# if res ==  n:
+#     print("Arm strong number")
+# else:
+#     print("Not an armstrong number")
+######################## 5 th page  donee ################################################
+# pattern printing - number pyramid 
+n = int(input("enter number of rowns:"))
+num=1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num+=1
+    print()
+
